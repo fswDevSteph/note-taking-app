c044148db92223f6af4623b68f2c5a8932d229e8
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-01-30 22:53:19 -0400

    imported modules and set router const

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,4 @@
+// define api endpoints with express router
+const express = require('express'); // requires express
+const router = express.Router(); // middllleware func allows defining routes for api and html .... and whatever route the application needs
+const fs = require('fs'); // to read and write data to db.json fille
