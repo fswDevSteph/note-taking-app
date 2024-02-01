c406cba2b2d8b9bfeca162e107a47537848c0774
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-02-01 19:51:47 -0400

    imported  the body-parser module to parse JSON request bodies

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express(); // this line creates a server once you run express()
 const PORT = process.env.PORT || 3000;
 const router = require('./routes/apiRoutes');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); //parses json request bodies
 app.use(router);
+
 app.listen(PORT, () => {
   console.log(`I am listening and Server is running on port ${PORT}`);
 });
