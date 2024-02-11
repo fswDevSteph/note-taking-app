18fa416ab161d0428ecb93c7cb43ef66ba10bcea
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-02-11 11:54:54 -0400

    i forget what i changed but see there are commmits to be made

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,7 @@ const express = require('express'); // requires express
 const router = express.Router(); // middllleware func allows defining routes for api and html .... and whatever route the application needs
 const fs = require('fs'); // to read and write data to db.json fille
 const util = require('util');
-const readFileUtil = util.promisify(fs.readFile)
+const readFileUtil = util.promisify(fs.readFile);
 //routes here
 
 //!GET portion of the route is for reading notes from the db.json file
@@ -13,16 +13,18 @@ router.get('/notes', (req, res) => {
   //http 'GET' route that makes a request to /notes 'END POINT' and takes 2 parms- request/response, those rep the incomming data
 
   //read notes from db.json
-  readFileUtil('db/db.json', 'utf8').then((data) => {
-    //reads contents of file located at db/db.json(this is the file path), utf8(this is the encording of the file type so it should be read as such), err(hold any error during file reading) data, (holds contents of the file thats read)  )
-    
-    //parse json data from file
-    const notes = JSON.parse(data); // json.parse is used to convert json data into a js object
+  readFileUtil('db/db.json', 'utf8')
+    .then((data) => {
+      //reads contents of file located at db/db.json(this is the file path), utf8(this is the encording of the file type so it should be read as such), err(hold any error during file reading) data, (holds contents of the file thats read)  )
 
-    //send notes as a response
-    res.json(notes); // .res (response object used to send HTTP responses back to the client), .json(notes)(formats the data in the notes variable as json and sends it as the response body to the client)
-    //! .catches pretty much always look like this and you should always have a .catch at the end of the .then (look for the transparent box)
-  }).catch((error) => res.status(500).json(error));
+      //parse json data from file
+      const notes = JSON.parse(data); // json.parse is used to convert json data into a js object
+
+      //send notes as a response
+      res.json(notes); // .res (response object used to send HTTP responses back to the client), .json(notes)(formats the data in the notes variable as json and sends it as the response body to the client)
+      //! .catches pretty much always look like this and you should always have a .catch at the end of the .then (look for the transparent box)
+    })
+    .catch((error) => res.status(500).json(error));
 });
 //!POST /notes: Adds a new note to the db.json file based on the data received in the request body.
 router.post('/notes', (req, res) => {
@@ -38,6 +40,7 @@ router.post('/notes', (req, res) => {
 
     //get new notye data from request body
     const newNote = req.body;
+    console.log(newNote);
 
     //add new note to array of existing notes
     notes.push(newNote);
@@ -55,4 +58,9 @@ router.post('/notes', (req, res) => {
     });
   });
 });
+
+router.delete('/notes/:id', (req, res) => {
+  console.log(req.params.id);
+});
+
 module.exports = router;
